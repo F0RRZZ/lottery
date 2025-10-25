c300a13a994fd6416302b80a32f3152fb9c48988
Gleb <tquicklyy@yandex.ru>
2025-10-25 12:39:10 +0300

    Add header #3

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, defineProps } from "vue";
+
+defineProps({
+    headerDivButtonText: String
+})
+</script>
+
+<template>
+    <div class="header-div">
+        <p class="header-p-logo">лотерея</p>
+        <button class="header-div-button">{{headerDivButtonText}}</button>
+    </div>
+</template>
+
+<style scoped>
+.header-div {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+}
+
+.header-p-logo {
+    font-size: 2em;
+    font-weight: bold;
+}
+
+.header-div-button {
+    background-color: #B25DFD;
+    color: white;
+    font-size: 1.5em;
+    border: 0px;
+    border-radius: 15px;
+    padding: 7px 30px 7px 30px;
+}
+</style>
