edd4428040b84bcc69398be705eaac9fde5f582f
Gleb <tquicklyy@yandex.ru>
2025-11-25 13:10:05 +0300

    Add user profile view #10

---
+++ frontend/src/components/Ticket.vue
@@ -0,0 +1,80 @@
+<script setup>
+import LottoPlayField from "./LottoPlayField.vue";
+
+defineProps({
+  ticket: Object,
+});
+</script>
+
+<template>
+  <div class="header-ticket-div">
+    <p class="ticket-name-p">Билет №{{ ticket.id }}</p>
+    <p
+      :class="
+        'status ' +
+        (ticket.status === 'win'
+          ? 'status-win'
+          : ticket.status === 'lose'
+          ? 'status-lose'
+          : 'status-not-played')
+      "
+      v-text="
+        ticket.status === 'win'
+          ? 'Победа'
+          : ticket.status === 'lose'
+          ? 'Неудача'
+          : 'В игре'
+      "
+    ></p>
+  </div>
+  <div class="play-fields-div">
+    <LottoPlayField :numbers="ticket.numbers.slice(0, 27)" />
+    <LottoPlayField :numbers="ticket.numbers.slice(27, 55)" />
+  </div>
+</template>
+
+<style scoped>
+.header-ticket-div {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 5px;
+  font-size: 1.5em;
+}
+
+.ticket-name-p {
+  font-weight: bold;
+  color: black;
+}
+
+.status {
+  width: 150px;
+  text-align: center;
+  font-size: 0.9em;
+  color: white;
+  padding: 2px 25px 2px 25px;
+  border-radius: 14px;
+}
+
+.status-win {
+  background-color: #00ce48;
+}
+
+.status-lose {
+  background-color: red;
+}
+
+.status-not-played {
+  background-color: #929292;
+}
+
+.play-fields-div {
+  display: flex;
+  flex-direction: column;
+  border: black solid 2px;
+  font-size: 1.5em;
+  padding: 10px;
+  gap: 10px;
+}
+</style>
---
+++ frontend/src/views/UserProfilePage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+import Header from "../components/HeaderWithButton.vue";
+import UserInformation from "../components/UserInformation.vue"
+import UserLotteriesList from "../components/UserLotteriesList.vue"
+import UserLotteriesListVue from "@/components/UserLotteriesList.vue";
+
+const headerDivButtonText = ref('На главную');
+const headerDivButtonLink = ref('/')
+
+</script>
+    
+<template>
+    <Header :headerDivButtonText="headerDivButtonText" :headerDivButtonLink="headerDivButtonLink"/>
+    <UserInformation />
+    <UserLotteriesList />
+</template>
